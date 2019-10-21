33a084b41c8f3389d67e3c7143fd2c1661db0731
hartsean <hartsean@gmail.com>
2019-10-21 16:18:13 +0000

    added node modules to .gitignore

--- studies/operators.js
+++ studies/operators.js
@@ -1,13 +1,25 @@
 /*
 * OPERATORS
 *
-* Assignment operators
+* 1. Assignment operators - 
+*    Assignment operators are used to assign values to JavaScript variables.
+*    The value to the right of the equal sign is passed to the variable on the right. 
+*    There are 6 Assignment operators:
+*
+*    var x = 5;
+*    var x += 5;
+*    var x -= 5;
+*    var x *= 5;
+*    var x /= 5;
+*    var x %= 5;
+*    
 * 
-* EX. var x = 5;
-* Arithmetic operators
+* 
+*    Comparison operators
+*    These operators compare two values.
 * 
 * 
-* Comparison operators
+*    
 * Logical operators
 * Unary operators (!, typeOf, -)
 * Ternary operator (a ? b : c)
