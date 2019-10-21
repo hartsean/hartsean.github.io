71deab614637893b1136391f055ceeee698c36f1
hartsean <hartsean@gmail.com>
2019-10-21 16:40:07 +0000

    deleted node modules from github

--- studies/loops.js
+++ studies/loops.js
@@ -1,5 +1,5 @@
 /*
-* LOOPS
+*    LOOPS
 * 
 *   
 * 
--- studies/operators.js
+++ studies/operators.js
@@ -12,19 +12,40 @@
 *    var x *= 5;
 *    var x /= 5;
 *    var x %= 5;
-*    
-* 
-* 
-*    Comparison operators
-*    These operators compare two values.
-* 
-* 
-*    
-* Logical operators
-* Unary operators (!, typeOf, -)
-* Ternary operator (a ? b : c)
-* 
+*   
 * 
 * 
 *  
 */
+
+// Comparison operators
+//    These operators compare two values.
+//     == 
+5 == "5"; // Equal to, but not strictly
+5 === 5;   // Strictly equal in value and type
+       
+>	greater than
+<   less than
+>=	greater than or equal to	
+<=	less than or equal to
+
+* Logical operators
+
+
+&&	and	(x < 10 && y > 1) is true
+||	or	(x === 5 || y === 5) is false
+!	not	!(x === y) is true
+
+* Unary operators (!, typeOf, -)
+! Bang
+typeOf 
+The typeof operator returns the type of a variable, object, function or expression:
+typeof "John"  // Returns string
+- delelete ?
+
+
+
+
+* Ternary operator (a ? b : c)
+
+
