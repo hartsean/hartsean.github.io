e5520832ad2ba0e0fc238e7d2e3219850d34f2c4
hartsean <hartsean@gmail.com>
2019-10-21 05:41:34 +0000

    studies complete, but need more examples

--- studies/functions.js
+++ studies/functions.js
@@ -1,41 +1,40 @@
 /*
 * FUNCTIONS
 *
-* 0. There are 12 different data types used in Javascript. They are listed and explained as follows:
-*
-* 1. Numbers - Numbers are a primitive data type, that consists of all real numbers.
-*    i.e. -1, 0, 1, 1.5, 24.98, and 1,000,000,000, etc.  They take up the largest amount of bit space 
-*    when stored as data by your program, compared to the other data types, at 8 bits. 
-* 2. Strings - Strings are a primititve data type that consists of a series of characters in quotation 
-*    marks. These strings can be manipluated (joined, spit, uppercased, etc) and their individual 
-*    characters can be accesed by using a zero index. 
-* 3. Boolean - A boolean is a datatype that is either true or false.  This is a primitive datatype that 
-*    and can be described as an on/off switch.  It's either 1 | 0.  It's also takes up the smallest
-*    amount of space when stored, coming in at one bit. 
-* 4. Array - an array is a complex datatype, that can store a set of ordered data consisting of other datatypes.
-*    arrays are deleniated with [], and can store strings numbers and other arrays within them seperated by commas. 
-*    Arrays can be accessed with using zero index. 
-* 5. Object - Objects are complex data types that hold data in key/value pairs. An object is deleniated
-*    with {}.  Within the brackets, they key/value pairs are seperated by commas. To access a value 
-*    stored within an object, you can call on it's key.  values can be any kind of datatype. 
-* 6. Functions - Functions are like little programs that do something by executing blocks of code when called by their name.
-*    decalre a funciton by using function name () {code to be executed}
-* 7. undefined - undefined is a datatype that has no definite value but has the space to hold a value. In other words,
-*    an object that has been decalred, but not initilized. 
-* 8. null - null is when the initilized object actually has no-value. 
-* 9. NaN - NaN is a acronym for Not a number. This is a data type that is similar to a number,
-*          a number data-type, but returns undefined.
-* 10.Infinity and -Infinity - Positive and Negative infinity is a value that is returned when a 
-*     number exceeds the largest possible representable number javascript can handle. Numbers can
-*    go on forever, but our computers wouldn't be able to handle that. So it returns this datatype. 
-*     
-* 11. What is the difference between primitive/simple and complex data types? 
-*     //Primitive data-types have a fixed size in bits.  Complex data types can be any size. 
-*     
-* 12. Primitive values are passed to a function BY COPY, complex values are BY REFERENCE. What does that mean, and how are they different?
-*     //When a primiteve datatype is used it's actual value (a fixed size) can be stored in the DOM. Complex data types
-*     //are only stored using their names as a reference to the location where the data is stored. 
-*     //becuase the actual data within them can be anysize,
-*     //by refrencing the location of a complex data type, we can access it's data selectively. 
-
+* 0. The two phases to using functions are the Creation phase and the execution phase.
+*    When you declare / create a function it is saved in the memory until it is called 
+*    by invoking it's name.  When it is called the code that is inside the code clock 
+*    is executed.
+*  
+* 1. A parameter is a variable in a method definition. When a method is called, the 
+*    arguments are the data you pass into the method's parameters. Parameter is variable 
+*    in the declaration of function. Argument is the actual value of this variable that gets 
+*    passed to function.  
+* 
+* 2. What’s the syntax for a NAMED function?
+*    
+*    function namedFunc(){};
+* 
+* 3. You an assign a function to a variable the same way you assign anything to a variable.
+*    var myVarFunc = function(){}; 
+*    OR you could use an arrow function
+*    
+*    var funcName = (params) => params + 2
+*    funcName(2); // returns 4
+* 
+* 4. Function CAN take inputs through it's parameters, that are passed through the function as
+*    arguments when the function is called at runtime, but they don't have to.
+*    If there are no parameters, the funciton will execute the code in the code block.
+* 
+*    When the code block is executed, it can include a return statement to return the result
+*    of the executed code to the console. But if no return statement, the function will just
+*    store the data.
+* 
+* 5. SCOPE.  Functions have their own local scope.  Variables inside the function can access 
+*    variables in the global scope. But the inverse is not true. 
+* 
+* 6. CLOSURES - A closure is the combination of a function bundled together (enclosed) with 
+*    references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function's scope from an inner function.
+*    
+* 
 */
